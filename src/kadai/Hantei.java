7c97e5be3734573f5fdb9517dcc92d97f55a759f
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-24 12:03:13 +0900

    使っていないクラスを削除

--- src/kadai/Hantei.java
+++ src/kadai/Hantei.java
@@ -9,14 +9,6 @@ public class Hantei {
         hitCount = 0;
     }
 
-    public void print(Player player,int[] num) {
-        System.out.println(player.name + "さんの予想結果");
-        System.out.print("HIT :" + hit.hantei(num,player.choice) + "\t"); //browを表示する
-        hitCount = hit.hantei(num,player.choice); //hitを保持する
-        System.out.println("BROW :" + blow.hantei(num,player.choice) + "\t");
-        System.out.println();
-    }
-
     public int getHitCount() {
         return hitCount;
     }
